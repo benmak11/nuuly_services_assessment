e730538e21101da99ce6e123eb9bc1df7c65fd32
Ben Makusha <btmakusha@duck.com>
2026-05-03 07:17:23 -0400

    feat[UPDATE]: Setup the project with javalin, SQLite, jackson and JUnit 5

--- src/main/java/app/inventory/Main.java
+++ src/main/java/app/inventory/Main.java
@@ -1,8 +1,19 @@
 package app.inventory;
 
+import app.inventory.db.Database;
+import io.javalin.Javalin;
 
 public class Main {
-    public static void main(String[] args) {
 
+    private static final int PORT = 7070;
+    private static final String DB_PATH = "inventory.db";
+
+    public static void main(String[] args) throws Exception {
+        Database database = Database.file(DB_PATH);
+        database.migrate();
+
+        Javalin app = Javalin.create().start(PORT);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(app::stop));
     }
 }
